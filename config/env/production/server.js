157f4842682f27d5f1673bff42bc8205141dc54d
CaptainPoldark <61985921+CaptainPoldark@users.noreply.github.com>
2022-03-17 02:45:09 -0400

    Update server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     url: env("MY_HEROKU_URL"),
     proxy: true,
     app: {
-      keys: env.array("APP_KEYS", [APP_KEY1, APP_KEY2, APP_KEY3, APP_KEY4]),
+      keys: env.APP.KEYS,
     },
   });
