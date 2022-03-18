931008ff4fcbe58b58d4d8e0e659201ff4615d96
CaptainPoldark <61985921+CaptainPoldark@users.noreply.github.com>
2022-03-17 22:47:36 -0400

    added Dev PGSQL db string and images
    added connection to the aws postgres db and removed public uploads from gitignore

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bank"),
+      user: env('DATABASE_USERNAME', "rxirgwhllvtjdk"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
-  },
+
+  }
 });
